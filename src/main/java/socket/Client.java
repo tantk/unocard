7986b67b7fb75c50103683f0c47c9bf91ebf29e6
tk <tantk7@gmail.com>
2016-08-06 19:31:11 +0800

    first commit

---
+++ src/main/java/socket/Client.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package socket;
+
+/**
+ *
+ * @author tantk
+ */
+public class Client {
+    private String name;
+    private String description;
+    private String type;
+    private String status;
+    private int id;
+
+    public String getName() {
+        return name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
